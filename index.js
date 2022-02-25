2f30b0ddc2125fb47abb7aaf6c6f9f52508e81d6
Sarah Williams <92603276+Sarahlophus@users.noreply.github.com>
2022-02-24 22:18:54 -0600

    added pseudocode and employee file constructors/functions

---
+++ index.js
@@ -0,0 +1,25 @@
+// global employee variable is empty array
+
+// requirements: employee, manager, engineer, intern
+
+// start employee profile generator - start inquirer
+
+// then block of inquirer (array of values/answers)
+
+// check answers.role
+
+// if role is 'Manager' create new manager object, and push into Employee array
+
+// if role is 'Engineer' create new engineer object, and push into Employee array
+
+// if role is 'Intern' create new intern object, and push into Employee array
+
+// if user wants to create another employee, call this function again
+
+// note: everything above is wrapped in a functions!
+
+// (new function) else create an HTML string literal. Note, if user doesn't continue, generate HTML function
+
+// inside of HTML loop through employees array to generate divs for cards
+
+// write employees to html file
