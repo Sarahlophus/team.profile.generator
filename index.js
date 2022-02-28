5627c09f0ad79a21f6a18e920d5d267b6a3d0086
Sarah Williams <92603276+Sarahlophus@users.noreply.github.com>
2022-02-27 23:23:04 -0600

    added funtions frameworks, inquirer questions, conditionals, and html template literal skeleton. Added css mock-up for site

--- index.js
+++ index.js
@@ -1,25 +1,129 @@
 // global employee variable is empty array
+let employeeArray = [];
+
+// requirements: packages
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 // requirements: employee, manager, engineer, intern
+const Employee = require("./Employee");
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
 
 // start employee profile generator - start inquirer
+function generateEmployee() {
+  inquirer
+    .prompt([
+      //questions here
+      {
+        name: "role",
+        type: "list",
+        message: "What is the role of this new employee?",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        name: "name",
+        type: "input",
+        message: "Enter employee's full name",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "Enter employee's email address",
+      },
+      {
+        name: "officeNumber",
+        type: "input",
+        message: "Enter Manager's office number",
+        when: (answers) => answers.role == "Manager",
+      },
+      {
+        name: "gitHub",
+        type: "input",
+        message: "Enter link to Engineer's github profile",
+        when: (answers) => answers.role == "Engineer",
+        validate: function (input) {
+          const valid = input.startsWith("https://");
+          return valid || "please enter a valid website that includes https://";
+        },
+      },
+      {
+        name: "school",
+        type: "input",
+        message: "Enter Intern's school",
+        when: (answers) => answers.role == "Intern",
+      },
+      {
+        name: "addNew",
+        type: "confirm",
+        message: "Would you like to add another employee?",
+      },
+    ])
 
-// then block of inquirer (array of values/answers)
+    // promise
+    .then((answers) => {
+      // check for employee ID
+      if (answers.role === "Manager") {
+        // if ID is 'Manager' create new manager object, and push into Employee array
+        let manager = new Manager(answers.id, answers.name, answers.email, answers.officeNumber);
+        employeeArray.push(manager);
+      }
 
-// check answers.role
+      if (answers.role === "Engineer") {
+        // if role is 'Engineer' create new engineer object, and push into Employee array
+        let engineer = new Engineer(answers.id, answers.name, answers.email, answers.gitHub);
+        employeeArray.push(engineer);
+      }
 
-// if role is 'Manager' create new manager object, and push into Employee array
+      if (answers.role === "Intern") {
+        // if role is 'Intern' create new intern object, and push into Employee array
+        let intern = new Intern(answers.id, answers.name, answers.email, answers.school);
+        employeeArray.push(intern);
+      }
 
-// if role is 'Engineer' create new engineer object, and push into Employee array
+      // if user wants to create another employee, call this function again
+      if (answers.addNew === true) {
+        generateEmployee();
+      } else generateHtml();
+    });
+}
 
-// if role is 'Intern' create new intern object, and push into Employee array
+//create an HTML string literal. Note, if user doesn't continue, generate HTML function
+const generateHtml = ({}) => {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>My Team</title>
+      <!-- bulma styling -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+  </head>
+  
+  <body>
+      <header class="hero has-background-primary-dark is-small">
+          <div class="hero-body">
+              <p class="has-text-centered has-text-white title">
+                  My Team
+              </p>
+          </div>
+      </header>
 
-// if user wants to create another employee, call this function again
-
-// note: everything above is wrapped in a functions!
-
-// (new function) else create an HTML string literal. Note, if user doesn't continue, generate HTML function
-
+    <body>
+    <section class="columns container is-centered is-multiline m-4">
+        ${generateEmployeeCards()}
+    </section>
+    </body>
+    </html>
+    `;
+};
 // inside of HTML loop through employees array to generate divs for cards
+function generateEmployeeCards() {
+  // loop though employe array for cards
+}
 
 // write employees to html file
+fs.writeFile("teamProfile.html", generateHtml(employeeArray), (err) => (err ? console.error(err) : console.log("you made a Team Profile!")));
